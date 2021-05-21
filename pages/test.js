99b0f020d85e530ee64b9795f61561d6431690b7
peter <yhuh@moneybrain.ai>
2021-05-22 05:43:33 +0900

    Google map api added

---
+++ pages/test.js
@@ -0,0 +1,14 @@
+import { latLng2Tile } from 'google-map-react';
+import Map from '../components/Map'
+
+
+const Test = () => {
+
+    return (
+        <div>
+            <Map/>
+        </div>
+    )
+}
+
+export default Test;
